94bff678153405f05c574dec5b93af96e6ef7a66
gurjotsingh4398 <gurjotsingh4398@gmail.com>
2019-01-04 08:23:55 -0800

    added image card

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,11 +1,18 @@
 var defferedPrompt;
 
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
 if ("serviceWorker" in navigator) {
   //register has 2nd argument ie. {scope: "/help"} which is scope to which service worker is applied.
   //this could be to the scope inside the scope og sw.js but not above it.
-  navigator.serviceWorker.register("/sw.js").then(() => {
-    console.log("Service Worker Registered");
-  });
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(() => {
+      console.log("Service Worker Registered");
+    })
+    .catch(err => console.log(err));
 }
 
 window.addEventListener("beforeinstallprompt", event => {
